7a9c24c1d41709f0f9935245bd6a5baabcd4362a
shakhawat-khan <satanfromhelll666@gmail.com>
2021-03-12 22:44:09 +0600

    Asynchronous code

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,10 +6,66 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+  void getData() async
+  {
+    // simulate network request for a username
+    
+     String username =   await Future.delayed(Duration(seconds: 3),(){
+
+      return 'yoshi';
+
+    });
+
+    String bio = await Future.delayed(Duration(seconds: 2),(){
+
+      return 'not a good boy';
+
+     });
+
+    print('$username -  $bio');
+  }
+
+  @override
+  void initState() {
+
+    super.initState();
+    print('init state');
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    print('build state');
+
+    //print('build function run');
+
     return Scaffold(
-      body: Text('choose location screen'),
+
+      backgroundColor: Colors.grey[200],
+
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('choose location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+
+
+      body: RaisedButton(
+        onPressed: ()
+            {
+              setState(() {
+                counter +=1;
+              });
+            },
+
+            child: Text('counter is $counter'),
+
+      ),
     );
   }
 }
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,6 +8,8 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
